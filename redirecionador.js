dee743f17e00fd478655942259487ba3934ad666
R.Frega <robertofrega@gmail.com>
2017-03-09 17:23:55 -0300

    feat(only first visit): add option to check if user visited before

--- redirecionador.js
+++ redirecionador.js
@@ -2,11 +2,41 @@ function redirecionador(url, options) {
   var defaultOptions = {
     timeout: 5,
     countdownElementId: 'countdown-message',
-    countdownTemplate: 'Você será redirecionado em #s'
+    countdownTemplate: 'Você será redirecionado em #s',
+    onlyFirstVisit: true
   };
+
   options = options || {};
   for (var option in defaultOptions) {
     options[option] = options[option] || defaultOptions[option]
+  }
+
+  if (options.onlyFirstVisit) {
+    function createCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
+
+    if (readCookie('visited')) {
+      return;
+    }
+    createCookie('visited', 'visited', 365)
   }
 
   var element = document.getElementById(options.countdownElementId);
